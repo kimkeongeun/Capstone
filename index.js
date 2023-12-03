b4eac19de9686803a30819ca3edc3c7595836ba8
kimsohyun0421 <147816705+kimsohyun0421@users.noreply.github.com>
2023-12-04 01:07:09 +0900

    node.js mySQL 연결

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const ejs = require('ejs')
+const app = express()
+const port = 3000
+const mysql = require('mysql')
+const { CONNREFUSED } = require('dns')
+
+// MySQL 데이터베이스 연결 설정
+const connection = mysql.createConnection({
+  host: 'classcatch.cafe24app.com', // HeidiSQL 호스트
+  user: 'classcatch', // HeidiSQL 유저네임
+  password: 'classdb2!', // HeidiSQL 비밀번호
+  database: 'classcatch' // 데이터베이스 이름
+});
+
+
+app.set('view engine', 'ejs')
+app.set('views','./views') 
+
+
+
+// 데이터베이스 연결
+connection.connect((err) => {
+  if (err) {
+    console.error('데이터베이스 연결 실패: ', err);
+    throw err;
+  }
+  console.log('데이터베이스 연결 성공!');
+});
+
+// 로그인 페이지 렌더링
+app.get('/', (req, res) => {
+  // 로그인 폼을 보여주는 HTML 페이지 렌더링
+  res.render('login');
+});
+
+// 데이터베이스 쿼리
+  connection.query('SELECT * from student', (error, rows, fields)=>{
+    if(error) throw error;
+    console.log('User info is:', rows);
+  });
+
+ 
+app.get('/guidepopup.ejs', (req, res) => {
+  // 가이드 폼을 보여주는 HTML 페이지 렌더링
+  res.render('guidepopup');
+});
+
+// 서버 실행
+app.listen(port, () => {
+  console.log(`서버가 http://localhost:${port} 에서 실행 중입니다.`);
+});
+
+
